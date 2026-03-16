79b70a60dc6b9c9acf309b718c4b23c3f1ef1396
axcel3399 <kanatkyzy30.2005@gmail.com>
2026-03-16 20:35:16 +0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,15 @@
 
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes)
+  ]
+};
+
+
 import { NgModule } from '@angular/core';
 
 @NgModule({

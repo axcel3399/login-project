110abb76341c2515e78deddc13b797dc8fbcc77c
axcel3399 <kanatkyzy30.2005@gmail.com>
2026-03-16 20:36:42 +0500

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,12 +1,10 @@
-// src/app/routes.ts
+import { Routes } from '@angular/router';
 import { LoginPageComponent } from './auth/login-page/login-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AuthGuard } from './auth/auth.guard';
-import { Routes } from '@angular/router';
 
-export const appRoutes: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
-  { path: 'home', component: HomePageComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: 'login' }
+  { path: 'home', component: HomePageComponent, canActivate: [AuthGuard] }
 ];
